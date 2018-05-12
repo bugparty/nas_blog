b6f611392edd0df1c8a844cfdab083c65be36bbc
bowman <fancycode@gmail.com>
2018-05-12 15:03:13 +0800

    mv contract to contract folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,27 @@
-var gulp = require('gulp');
-var minify = require('gulp-minify');
-var replace = require('gulp-replace');
-var clipboard = require("gulp-clipboard");
+var gulp = require('gulp')
+var minify = require('gulp-minify')
+var replace = require('gulp-replace')
+var clipboard = require('gulp-clipboard')
 
-gulp.task('compress', function() {
+gulp.task('compress', function () {
   gulp.src('src/contract.js')
     .pipe(minify())
     .pipe(replace('"', '\\"'))
     .pipe(clipboard())
     .pipe(gulp.dest('dist'))
+})
 
-});
-
-gulp.task('compress2', function() {
-    gulp.src('src/contract.js')
-      .pipe(minify({
-          ext:{
-              src:'-debug.js',
-              min:'.js'
-          },
-          exclude: ['tasks'],
-          ignoreFiles: ['.combo.js', '-min.js']
-      }))
-      .pipe(gulp.dest('dist'))
-      .pipe(replace('"', '\\"'))
-      .pipe(clipboard())
-  
-  
-  });
-
+gulp.task('compress2', function () {
+  gulp.src('src/contract.js')
+    .pipe(minify({
+      ext: {
+        src: '-debug.js',
+        min: '.js'
+      },
+      exclude: ['tasks'],
+      ignoreFiles: ['.combo.js', '-min.js']
+    }))
+    .pipe(gulp.dest('dist'))
+    .pipe(replace('"', '\\"'))
+    .pipe(clipboard())
+})
